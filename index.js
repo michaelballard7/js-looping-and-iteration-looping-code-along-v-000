543f36da5c2459f7e1c176098ac0ca2da3a08b5c
Michael Ballard <ballard.michael9@gmail.com>
2019-07-03 00:55:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function writeCards(arr, eventName){
     let newArr = new Array();
-    for(let i = 0; i<arr.length-1; i++){
+    for(let i = 0; i<arr.length; i++){
         newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift.`)
     }
     return newArr
