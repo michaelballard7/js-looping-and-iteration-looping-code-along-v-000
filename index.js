de0421d772027f1b318b4cb04339ac6142f3d5a2
Michael Ballard <ballard.michael9@gmail.com>
2019-07-03 01:07:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(arr, eventName){
+    let newArr = new Array();
+    for(let i = 0; i<arr.length; i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArr
+}
+
+function countdown(int){
+    while(int > 0){
+        console.log(int);
+        int -=1
+    }
+    console.log(int)
+}
